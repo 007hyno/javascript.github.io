da0715103d6bb1ae5e1d20b1fdb18bbe567ec755
Paras <68294675+007hyno@users.noreply.github.com>
2020-10-30 13:25:30 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,34 +1,42 @@
 setInterval(function(){ 
-    let t = new Date();
-    n = (t.getHours())
-    mm = (t.getMinutes())
-    ss = (t.getSeconds())
+  let t = new Date();
+  n = (t.getHours())
+  mm = (t.getMinutes())
+  ss = (t.getSeconds())
+  if(n>12){ 
     hh = h12(n);
-    tt = ampm(n);
-// console.log(t.getHours());  
-// console.log(t.getMinutes());  
-// console.log(t.getSeconds());
-document.getElementById('hh').innerHTML=hh;
-document.getElementById('mm').innerHTML=mm;
+    document.getElementById('hh').innerHTML=hh;
+  }else{
+    document.getElementById('hh').innerHTML=n;
+  }
+  console.log('h12 is here : '+h12(n));
+  console.log('hh is here : '+hh);
+  tt = ampm(n);
+  // console.log(t.getHours());  
+  // console.log(t.getMinutes());  
+  // console.log(t.getSeconds());
+  document.getElementById('mm').innerHTML=mm;
 document.getElementById('ss').innerHTML=ss;
 document.getElementById('tt').innerHTML=tt;
 }, 1000);
 
 // 12 hours formate logic here
 h12 = (h)=>{
-    if(h>12){
-        r=h-12;
-        return r
+  if(h>12){
+      r=h-12;
+      return r
     }
-}
-// Am Pm logic here
-ampm = (h)=>{
-    if(h>12){
-        a='PM'
-        return a
+  }
+  // Am Pm logic here
+  ampm = (hhh)=>{
+    //q=parseInt(hhh);
+    //console.log('q is here : '+q)
+    if(hhh==12&&mm>0){
+      return 'PM';
     }
-    else{
-        a='AM'
-        return a
+    else if(hhh>12){
+      return 'PM';
+    }else{
+      return 'AM';
     }
-}
+  }
